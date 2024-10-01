f9f4cda598c4bf3380e3a414d27170d51746b480
Kalinin <dvport.g@gmail.com>
2024-10-01 20:15:58 +0400

    ConstantFunction + expanding UnitFunction, ZeroFunction

---
+++ Lab2/src/main/java/functions/ConstantFunction.java
@@ -0,0 +1,16 @@
+package functions;
+public class ConstantFunction implements MathFunction {
+
+    private final double C;
+
+    public ConstantFunction(double C) {
+        this.C = C;
+    }
+
+    public double apply(double x) {
+        return C;
+    }
+    public double getC() {
+        return C;
+    }
+}
---
+++ Lab2/src/main/java/functions/UnitFunction.java
@@ -0,0 +1,6 @@
+package functions;
+public class UnitFunction extends ConstantFunction{
+    public UnitFunction(){
+        super(1);
+    }
+}
