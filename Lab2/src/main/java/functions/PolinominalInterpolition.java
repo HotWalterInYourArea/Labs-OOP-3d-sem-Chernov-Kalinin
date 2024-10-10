7af6f7f4440dcd9c75dfe49e0f383c569f1b7294
Kalinin <dvport.g@gmail.com>
2024-10-10 14:13:29 +0400

    correct PolinominalInterpolition

--- Lab2/src/main/java/functions/PolinominalInterpolition.java
+++ Lab2/src/main/java/functions/PolinominalInterpolition.java
@@ -1,55 +1,28 @@
 package functions;
-import java.util.Scanner;
 public class PolinominalInterpolition implements MathFunction {
+    private double[] xValues = new double[4];
+    private double[] yValues = new double[4];
+    private int count = xValues.length;
+    public PolinominalInterpolition(double[] xValues, double[] yValues){
+        this.xValues = xValues;
+        this.yValues = yValues;
+    }
         public double apply ( double x){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Введите число:");
 
-            int n = sc.nextInt();
-            if (n <= 1) {
-                System.out.println("Введено некорректное значение");
-                return (-1);
-            } else {
-
-                double[] xz = new double[n];
-                for (int i = 0; i <= n - 1; i++) {
-                    Scanner sd = new Scanner(System.in);
-                    System.out.println("Введите число xz, абсциссу:");
-                    xz[i] = sd.nextDouble();
-                }
-                if ((x < xz[0]) || (x > xz[n - 1])) {
-                    System.out.println("Число x не принадлежит области определения функции y");
-                    return -1;
-                } else {
-                    for (int i = 0; i <= n - 1; i++) {
-                        if (x == xz[i]) {
-                            System.out.println("x совпал с узлом интерполяции");
-                            return -1;
-                        }
-                    }
-                    double[] y = new double[n];
-                    for (int i = 0; i <= n - 1; i++) {
-                        Scanner sg = new Scanner(System.in);
-                        System.out.println("Введите число y, ординату:");
-                        y[i] = sg.nextDouble();
-                    }
                     double S = 0;
-                    for (int i = 0; i <= n - 1; i++) {
+                    for (int i = 0; i <= count - 1; i++) {
                         double tn = 1;
                         double d = 1;
-                        for (int j = 0; j <= n - 1; j++) {
+                        for (int j = 0; j <= count - 1; j++) {
                             if (j == i) continue;
-                            tn = tn * (x - xz[j]);
-                            d = d * (xz[i] - xz[j]);
+                            tn = tn * (x - xValues[j]);
+                            d = d * (xValues[i] - xValues[j]);
                         }
                         double ir = tn / d;
-                        S = S + y[i] * ir;
+                        S = S + yValues[i] * ir;
                     }
-                    System.out.println(S);
                     return S;
                 }
-            }
-        }
     }
 
 
