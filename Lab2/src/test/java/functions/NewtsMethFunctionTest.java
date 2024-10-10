aa75efb04805a1f8e76612e8f64d96339d400134
CHERNOV KLIM <klimcherno@gmail.com>
2024-10-10 16:49:56 +0400

    NewtsMethTest update and minor changes to NewtsMeth

--- Lab2/src/test/java/functions/NewtsMethFunctionTest.java
+++ Lab2/src/test/java/functions/NewtsMethFunctionTest.java
@@ -14,20 +14,26 @@ class NewtsMethFunctionTest {
     private final NewtsMethFunction t_func1=new NewtsMethFunction(sin,1);
     @Test
     void applyNewtsMethToSin_ExpectTrue_LowPrecision(){
-        assertTrue(Math.abs(Math.PI-t_func1.apply(2))<1);
+        assertEquals(Math.PI,t_func1.apply(2),1);
     }
     private final NewtsMethFunction t_func2=new NewtsMethFunction(sin,1e-6);
     @Test
     void applyNewtsMethToSin_ExpectTrue_HighPrecision(){
-        assertTrue(Math.abs(Math.PI-t_func2.apply(2))<1e-6);
+        assertEquals(Math.PI,t_func2.apply(2),1e-6);
     }
     @Test
     void applyNewtsMethToSin_NotConverge_DerivIsZero(){
         assertTrue(Double.isNaN(t_func2.apply(Math.PI/2)));
     }
+    private final NewtsMethFunction t_func3=new NewtsMethFunction(new LinkedListTabulatedFunction(new double[] {1.0,2.0},new double[]{0,4.0}),1);
     @Test
-    void applyNewtsMethToSin_ExpectFalse_LowPrecision(){
-        assertTrue(Math.abs(Math.PI-t_func1.apply(2))<1);
+    void applyNewtsMethToTabulated_ExpectTrue_LowPrecision(){
+        assertEquals(1,t_func3.apply(2),1);
+    }
+    private final NewtsMethFunction t_func4=new NewtsMethFunction(new LinkedListTabulatedFunction(new double[] {1.0,2.0},new double[]{0,4.0}),1e-6);
+    @Test
+    void applyNewtsMethToTabulated_ExpectTrue_HighPrecision(){
+        assertEquals(1,t_func3.apply(2),1e-6);
     }
 
 }
