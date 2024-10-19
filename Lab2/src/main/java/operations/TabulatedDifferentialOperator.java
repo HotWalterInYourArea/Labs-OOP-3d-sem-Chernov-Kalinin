7ffbdc0156ecfa22aaee40993d35d8d51eed4fa9
CHERNOV KLIM <klimcherno@gmail.com>
2024-10-19 16:13:09 +0400

    added DifferentialOperator, TabulatedDifferentialOperator

---
+++ Lab2/src/main/java/operations/TabulatedDifferentialOperator.java
@@ -0,0 +1,39 @@
+package operations;
+
+import functions.Point;
+import functions.TabulatedFunction;
+import functions.factory.ArrayTabulatedFunctionFactory;
+import functions.factory.TabulatedFunctionFactory;
+
+public class TabulatedDifferentialOperator implements DifferentialOperator<TabulatedFunction>{
+    TabulatedFunctionFactory factory;
+    TabulatedDifferentialOperator(TabulatedFunctionFactory fac){
+        this.factory=fac;
+    }
+    TabulatedDifferentialOperator(){
+        this.factory=new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public TabulatedFunction derive(TabulatedFunction func){
+        int count=func.getCount();
+        Point[] p=TabulatedFunctionOperationService.asPoints(func);
+        double[] xValues=new double[count];
+        double[] yValues=new double[count];
+        for(int i=0;i<count-1;i++){
+            xValues[i]=p[i].x;
+            yValues[i]=p[i+1].y/(p[i+1].x-p[i].x)-p[i].y/(p[i+1].x-p[i].x);
+        }
+        xValues[count-1]=p[count-1].x;
+        yValues[count-1]=yValues[count-2];
+        return factory.create(xValues,yValues);
+    }
+}
