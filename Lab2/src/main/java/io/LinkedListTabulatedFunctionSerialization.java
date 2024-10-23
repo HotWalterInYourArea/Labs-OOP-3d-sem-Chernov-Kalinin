61c0785502865110d0f38b7a29ebdd1378668d4b
CHERNOV KLIM <klimcherno@gmail.com>
2024-10-24 03:39:15 +0400

    minor fixes

--- Lab2/src/main/java/io/LinkedListTabulatedFunctionSerialization.java
+++ Lab2/src/main/java/io/LinkedListTabulatedFunctionSerialization.java
@@ -10,18 +10,19 @@ import java.io.*;
 public class LinkedListTabulatedFunctionSerialization {
 
     public static void main(String[] args){
-//            try(BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(new FileOutputStream("Lab2/output/serialized linked list function.bin"))){
-//                TabulatedFunction func = new LinkedListTabulatedFunction(new double[]{0, 1, 4, 5}, new double[]{8, 4, 9, 3});
-//                TabulatedDifferentialOperator diffOp=new TabulatedDifferentialOperator(new LinkedListTabulatedFunctionFactory());
-//                TabulatedFunction nu_func=diffOp.derive(func);
-//                TabulatedFunction nuer_func=diffOp.derive(nu_func);
-//                FunctionsIO.serialize(bufferedOutputStream,func);
-//                FunctionsIO.serialize(bufferedOutputStream,nu_func);
-//                FunctionsIO.serialize(bufferedOutputStream,nuer_func);
-//            }catch(IOException e){
-//                e.printStackTrace();
-//            }
-            try(BufferedInputStream bufferedInputStream=new BufferedInputStream(new FileInputStream("Lab2/output/linked list.bin"))){
+            try(BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(new FileOutputStream("Lab2/output/serialized linked list function.bin"))){
+                LinkedListTabulatedFunction func = new LinkedListTabulatedFunction(new double[]{0, 1, 4, 5}, new double[]{8, 4, 9, 3});
+                TabulatedDifferentialOperator diffOp=new TabulatedDifferentialOperator(new LinkedListTabulatedFunctionFactory());
+                var nu_func = (LinkedListTabulatedFunction)diffOp.derive(func);
+                var nuer_func=(LinkedListTabulatedFunction)diffOp.derive(nu_func);
+                FunctionsIO.serialize(bufferedOutputStream,func);
+                FunctionsIO.serialize(bufferedOutputStream,nu_func);
+                FunctionsIO.serialize(bufferedOutputStream,nuer_func);
+                bufferedOutputStream.flush();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            try(BufferedInputStream bufferedInputStream=new BufferedInputStream(new FileInputStream("Lab2/output/serialized linked list function.bin"))){
                 System.out.println(FunctionsIO.deserialize(bufferedInputStream).toString());
                 System.out.println(FunctionsIO.deserialize(bufferedInputStream).toString());
                 System.out.println(FunctionsIO.deserialize(bufferedInputStream).toString());
